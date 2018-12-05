b2ba91e54a00b9e61441fcaccad99d1f4e7c9a47
Hauke Hans <h.hans@emetriq.com>
2018-12-05 13:14:15 +0100

    day5 p2

--- day5.py
+++ day5.py
@@ -1,12 +1,26 @@
+import string
+import sys
+
 with open('in/day5a.txt') as f:
     raw_in = f.readline().strip()
 
-i = 0
-while i < len(raw_in) - 1:
-    if ord(raw_in[i]) == (ord(raw_in[i + 1]) + 32) or ord(raw_in[i]) == (ord(raw_in[i + 1]) - 32):
-        raw_in = raw_in[:i] + raw_in[i + 2:]
-        i = max(0, i - 2)
-    else:
-        i = i + 1
 
-print(len(raw_in))
+def collapse(word):
+    i = 0
+    while i < len(word) - 1:
+        if ord(word[i]) == (ord(word[i + 1]) + 32) or ord(word[i]) == (ord(word[i + 1]) - 32):
+            word = word[:i] + word[i + 2:]
+            i = max(0, i - 2)
+        else:
+            i = i + 1
+    return word
+
+
+print(len(collapse(raw_in)))
+
+min_len = sys.maxsize
+
+for cc in string.ascii_lowercase:
+    min_len = min(len(collapse(''.join([c for c in raw_in if c.lower() != cc]))), min_len)
+
+print(min_len)
